dace99fac739d9fcf820ff6e54fd563445882375
Veries Seals III <veries@me.com>
2023-01-26 12:15:23 -0600

    Added this.question

--- js/app.js
+++ js/app.js
@@ -1,5 +1,70 @@
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
+class Trivia {
+    constructor() {
+        // Get Access to Buttons in HTML
+        this.startButton = document.getElementById('startBtn');
+        this.nextButton = document.getElementById('nextBtn');
+        this.questionContainerElement = document.getElementById('question-container');
+        this.questionElement = document.getElementById('question');
+        this.answerButtonsElement = document.getElementById('answer-buttons');
+        this.questionElement = document.getElementById('question');
+        this.answerButtonsElement = document.getElementById('answer-buttons');
 
-// ctx.fillStyle = "grey";
-// ctx.fillRect(10, 10, 150, 100)
+        // Array of Questions
+        // Each Question is an Object
+        // ----------------------------------------------------
+        this.questions = [
+            // ----------------------------------------------------
+            // Question #1
+            // ----------------------------------------------------
+            {
+                question: 'What is 2 + 2?',
+                answers: [
+                    { text: '4', correct: true },
+                    { text: '22', correct: false }
+                ]
+            }
+            // ----------------------------------------------------
+            // Question #2
+            // ----------------------------------------------------
+        ]
+    }
+
+    // Init Method
+    // This method will be called when the page loads
+    // ----------------------------------------------------
+    init() {
+        // Add Event Listener to Start Button
+        this.startButton.addEventListener('click', () => {
+            this.startGame();
+        });
+
+        this.shuffledQuestions
+        this.currentQuestionIndex; // Variables to be used later
+    }
+
+    
+
+    // Start Game Method
+    // This method will be called when the start button is clicked
+    // ----------------------------------------------------
+    startGame() {
+        console.log('Started');
+        this.startButton.classList.add('hide');
+        this.shuffledQuestions = this.questions.sort(() => Math.random() - .5);
+        this.currentQuestionIndex = 0;
+        this.questionContainerElement.classList.remove('hide');
+        this.setNextQuestion();
+    }
+
+    // Set Next Question Method
+    // ----------------------------------------------------
+    setNextQuestion() {
+        this.showQuestion(this.shuffledQuestions[currentQuestionIndex]);
+    }
+
+    
+
+}
+
+const action = new Trivia();
+action.init();
